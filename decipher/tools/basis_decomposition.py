42b31c9b94fcd3e66c50ef71dfac9b323c7348e0
ANazaret <achille.nazaret@gmail.com>
2023-11-01 16:10:42 -0400

    support uncertainty in the computation of disruption scores

--- decipher/tools/basis_decomposition.py
+++ decipher/tools/basis_decomposition.py
@@ -1,3 +1,5 @@
+import logging
+
 import numpy as np
 import pandas as pd
 
@@ -8,13 +10,19 @@ from decipher.tools._basis_decomposition.run import (
     get_basis,
 )
 
+logger = logging.getLogger(__name__)
+logging.basicConfig(
+    format="%(asctime)s | %(levelname)s : %(message)s",
+    level=logging.INFO,  # stream=sys.stdout
+)
+
 
 def basis_decomposition(
     adata,
     pattern_names=None,
     n_basis=5,
     n_iter=10_000,
-    lr=1e-2,
+    lr=5e-3,
     beta_prior=1,
     seed=0,
 ):
@@ -43,9 +51,10 @@ def basis_decomposition(
     )
     gene_scales = gene_scales.detach().numpy()
     basis = get_basis(trajectory_model, guide, gene_patterns, times)
+    betas_mean = samples["beta"]["mean"].squeeze().detach().numpy()
     adata.uns["decipher"]["basis_decomposition"] = {
         "scales": gene_scales,
-        "betas": samples["beta"]["mean"].squeeze().detach().numpy(),
+        "betas": betas_mean,
         "betas_samples": samples["beta"]["values"].squeeze().detach().numpy(),
         "basis": basis,
         "times": times.detach().numpy(),
@@ -57,41 +66,92 @@ def basis_decomposition(
         "pattern_names": pattern_names,
     }
     for i, p_name in enumerate(pattern_names):
-        adata.varm[f"decipher_betas_{p_name}"] = betas[i, :, :]
+        adata.varm[f"decipher_betas_{p_name}"] = betas_mean[i, :, :]
 
     return losses
 
 
-def disruption_scores(adata):
+def disruption_scores(adata, pattern_name_a=0, pattern_name_b=1):
     """Compute all possible disruption scores:
     - shape: ||beta[0] - beta[1]||_2
     - scale: | log(s[0]) - log(s[1]) |
     - combined: || log(beta[0]*s[0]) - log(beta[1]*s[1]) ||
 
     """
-    disruptions = []
-    for g_id in range(len(adata.var_names)):
-        beta_g = adata.uns["decipher"]["basis_decomposition"]["betas"][:, g_id, :]
-        gene_scales = adata.uns["decipher"]["basis_decomposition"]["scales"][:, g_id]
-
-        shape_disruption = np.linalg.norm(beta_g[0] - beta_g[1], ord=2)
-        scale_disruption = abs(np.log(gene_scales[0]) - np.log(gene_scales[1]))
-        combined_disruption = np.linalg.norm(
-            np.log(gene_scales[0] * beta_g[0]) - np.log(gene_scales[1] * beta_g[1]), ord=2
+    if type(pattern_name_a) == str:
+        pattern_name_a = adata.uns["decipher"]["basis_decomposition"]["pattern_names"].index(
+            pattern_name_a
         )
-
-        disruptions.append(
-            (
-                adata.var_names[g_id],
-                shape_disruption,
-                scale_disruption,
-                combined_disruption,
-            )
+    if type(pattern_name_b) == str:
+        pattern_name_b = adata.uns["decipher"]["basis_decomposition"]["pattern_names"].index(
+            pattern_name_b
         )
-    disruptions = pd.DataFrame(
-        disruptions, columns=["gene", "shape", "scale", "combined"]
+    idx_a = pattern_name_a
+    idx_b = pattern_name_b
+    disruptions_mean = []
+
+    def pairwise_distances(x, y):
+        """
+        Parameters
+        ----------
+        x : array_like
+            An array of m samples of k variables with d dimensions. Shape: (m, k, d)
+        y : array_like
+            An array of n samples of k variables with d dimensions. Shape: (n, k, d)
+        Returns
+        -------
+        distance : ndarray
+            The matrix of all pairwise distances, shape = (m, n, k).
+        """
+        x = np.expand_dims(x, axis=1)  # shape: (m, 1, k, d)
+        y = np.expand_dims(y, axis=0)  # shape: (1, n, k, d)
+        return np.linalg.norm(x - y, ord=2, axis=-1)
+
+    # betas_samples shape: (n_samples, n_patterns, n_genes, n_times)
+    beta_a = adata.uns["decipher"]["basis_decomposition"]["betas_samples"][:, idx_a, :, :]
+    beta_b = adata.uns["decipher"]["basis_decomposition"]["betas_samples"][:, idx_b, :, :]
+    # (gene) scales shape: (n_patterns, n_genes)
+    gene_scales_a = adata.uns["decipher"]["basis_decomposition"]["scales"][idx_a, :]
+    gene_scales_b = adata.uns["decipher"]["basis_decomposition"]["scales"][idx_b, :]
+
+    n_genes = len(adata.var_names)
+    shape_disruption = pairwise_distances(beta_a, beta_b)
+    shape_disruption = shape_disruption.reshape(-1, n_genes)
+    scale_disruption = np.abs(np.log(gene_scales_a) - np.log(gene_scales_b))
+    combined_disruption = pairwise_distances(
+        np.log(gene_scales_a[None, :, None] * beta_a),
+        np.log(gene_scales_b[None, :, None] * beta_b),
+    )
+    combined_disruption = combined_disruption.reshape(-1, n_genes)
+    disruptions_mean = pd.DataFrame(
+        {
+            "gene": adata.var_names,
+            "shape": shape_disruption.mean(axis=0),
+            "scale": scale_disruption,
+            "combined": combined_disruption.mean(axis=0),
+        }
     ).set_index("gene")
-    adata.var["decipher_disruption_shape"] = disruptions["shape"]
-    adata.var["decipher_disruption_scale"] = disruptions["scale"]
-    adata.var["decipher_disruption_combined"] = disruptions["combined"]
-    return disruptions
+
+    adata.var["decipher_disruption_shape"] = disruptions_mean["shape"]
+    adata.var["decipher_disruption_scale"] = disruptions_mean["scale"]
+    adata.var["decipher_disruption_combined"] = disruptions_mean["combined"]
+    logger.info("Added `.var['decipher_disruption_shape']`: shape disruption scores")
+    logger.info("Added `.var['decipher_disruption_scale']`: scale disruption scores")
+    logger.info("Added `.var['decipher_disruption_combined']`: combined disruption scores")
+
+    n_samples = shape_disruption.shape[0]
+    # repeat the gene names n_samples times
+    disruptions_samples = pd.DataFrame(
+        {
+            "gene": np.tile(adata.var_names, n_samples),
+            "shape": shape_disruption.reshape(-1),
+            "combined": combined_disruption.reshape(-1),
+        }
+    )
+    adata.uns["decipher"]["disruption_scores_samples"] = disruptions_samples
+    adata.uns["decipher"]["disruption_scores"] = disruptions_mean
+    logger.info("Added `.uns['decipher']['disruption_scores']`: disruption scores")
+    logger.info(
+        "Added `.uns['decipher']['disruption_scores_samples']`: disruption scores probabilistic "
+        "samples"
+    )
