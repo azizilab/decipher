35157e81c62e9f4205d8911c0838dbd63f9cd7d2
Achille Nazaret <achille.nazaret@gmail.com>
2023-11-13 16:44:34 -0500

    expose function in __init__

--- decipher/tools/__init__.py
+++ decipher/tools/__init__.py
@@ -7,7 +7,12 @@ from .trajectory_inference import (
     TConfig,
 )
 from .basis_decomposition import basis_decomposition, disruption_scores
-from decipher.tools.decipher import decipher_train, decipher_rotate_space, decipher_gene_imputation
+from decipher.tools.decipher import (
+    decipher_train,
+    decipher_rotate_space,
+    decipher_gene_imputation,
+    decipher_and_gene_covariance,
+)
 from ._decipher.data import decipher_load_model
 from ._decipher.decipher import DecipherConfig
 
